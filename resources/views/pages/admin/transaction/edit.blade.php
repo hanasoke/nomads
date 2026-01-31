e2bf09b88d380cace375eeaf07eb99bd43309dfb
hanasbp <hanasoke@gmail.com>
2026-01-31 09:56:17 +0700

    added update transaction

--- resources/views/pages/admin/transaction/edit.blade.php
+++ resources/views/pages/admin/transaction/edit.blade.php
@@ -21,48 +21,21 @@
 
     <div class="card shadow">
         <div class="card-body">
-            <form action="{{ route('travel-package.update', $item->id) }}" method="post">
+            <form action="{{ route('transaction.update', $item->id) }}" method="post">
                 @method('PUT')
                 @csrf 
                 <div class="form-group">
-                    <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ $item->title }}" >
-                </div>
-                <div class="form-group">
-                    <label for="location">Location</label>
-                    <input type="text" class="form-control" id="location" name="location" placeholder="Location" value="{{ $item->location }}">
-                </div>
-                <div class="form-group">
-                    <label for="about">About</label>
-                    <textarea name="about" id="about" rows="10" class="d-block w-100 form-control">{{ $item->about }}</textarea>
-                </div>
-                <div class="form-group">
-                    <label for="featured_event">Featured Event</label>
-                    <input type="text" class="form-control" id="featured_event" name="featured_event" placeholder="Featured Event" value="{{ $item->featured_event }}">
-                </div>
-                <div class="form-group">
-                    <label for="language">Language</label>
-                    <input type="text" class="form-control" id="language" name="language" placeholder="Language" value="{{ $item->language }}">
-                </div>
-                <div class="form-group">
-                    <label for="foods">Foods</label>
-                    <input type="text" class="form-control" id="foods" name="foods" placeholder="Foods" value="{{ $item->foods }}">
-                </div>
-                <div class="form-group">
-                    <label for="departure_date">Departure Date</label>
-                    <input type="date" class="form-control" id="departure_date" name="departure_date" placeholder="Departure Date" value="{{ $item->departure_date }}">
-                </div>
-                <div class="form-group">
-                    <label for="duration">Duration</label>
-                    <input type="text" class="form-control" id="duration" name="duration" placeholder="Departure Date" value="{{ $item->duration }}">
-                </div>
-                <div class="form-group">
-                    <label for="type">Type</label>
-                    <input type="text" class="form-control" id="type" name="type" placeholder="Type" value="{{ $item->type }}">
-                </div>
-                <div class="form-group">
-                    <label for="price">Price</label>
-                    <input type="number" class="form-control" id="price" name="price" placeholder="Price" value="{{ $item->price }}">
+                    <label for="transaction_status">Status</label>
+                    <select name="transaction_status" required class="form-control">
+                        <option value="{{ $item->transaction_status }}">
+                            Jangan Ubah ({{ $item->transaction_status }})
+                        </option>
+                        <option value="IN_CART">In Cart</option>
+                        <option value="PENDING">Pending</option>
+                        <option value="SUCCESS">Success</option>
+                        <option value="CANCEL">Cancel</option>
+                        <option value="FAILED">Failed</option>
+                    </select>
                 </div>
                 <button type="submit" class="btn btn-primary btn-block">
                     Ubah
